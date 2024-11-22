03c56326678cff7203b34dc43c4305d97e9dcb31
Rupesh Kumar <rkn0907@gmail.com>
2024-11-22 11:21:05 +0530

    heading

---
+++ src/components/models/HeaderSecond.js
@@ -0,0 +1,23 @@
+import React from "react";
+
+function HeaderSecond() {
+  return (
+    <div>
+      <ul>
+        <li>
+          <img
+            src="https://s3-alpha-sig.figma.com/img/cbd9/75b3/772f76bc0d9f8f77fc437be015a23a00?Expires=1733097600&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=Mi2aMjCfiBR2Ol2mVv5xpgjYAwZMnJiLmoOMUdVm0JVqXf~XclbsjBV4uZriwovfEfHctKzi0wA4nx1Tdj5-KMaMLGzdtn2xBqVouRuL8~sNUdtGX~5n468dW8MYBMV53~dviM2-s5TtEscLBjcj24e067C3Po7oSXnJeocsj2Sr0akufDldh4zmIaofzvOhZMT8sItcoWb4x-U31P12ObIKIicUaRJp1C-d6~yr-aF5d8xqxKUsleBPCFviudogtTSjsTMrGgSyK5dC066bEXrPbjkgZva8Q4yP2zJOz6T78rLGpQ0zjPIe3NKWO0sMB-1w8Vme~KgEqoyQ5nEMrg__"
+            alt="person"
+          />
+          <strong>Rahul Yadav</strong>
+        </li>
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
+      </ul>
+    </div>
+  );
+}
+
+export default HeaderSecond;
